083995ce60a8e37bbf5c8f9d7fdd80436e987b9e
SHAILESH BISHT <peskyji@gmail.com>
2019-06-19 10:42:38 +0530

    chat application work Chathome activity updated fragment added

--- app/src/main/java/e/nanu/graphian2/ChatHomeActivity.java
+++ app/src/main/java/e/nanu/graphian2/ChatHomeActivity.java
@@ -1,6 +1,8 @@
 package e.nanu.graphian2;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,13 +12,23 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 
 public class ChatHomeActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
     private Toolbar mToolbar;
 
+    private ViewPager mViewPager;
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private DatabaseReference mUserRef;
+
+    private TabLayout mTabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +39,26 @@ public class ChatHomeActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("GRAPHIAN");
+
+        /*if (mAuth.getCurrentUser() != null) {
+
+
+            mUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+
+        }*/
+
+
+        //Tabs
+        mViewPager = (ViewPager) findViewById(R.id.main_tabPager);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+
+
     }
 
     public void onStart() {
