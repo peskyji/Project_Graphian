11357682775860009a439abe1a70837070ba46e5
SHAILESH BISHT <peskyji@gmail.com>
2019-06-19 10:43:47 +0530

    chat application work Chathome activity updated fragment added

---
+++ app/src/main/java/e/nanu/graphian2/SectionsPagerAdapter.java
@@ -0,0 +1,61 @@
+package e.nanu.graphian2;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    // this method return the position of each tab so we instantiate each fragment for each tab
+    public Fragment getItem(int position) {
+
+        switch(position) {
+            case 0:
+                RequestFragment requestFragment = new RequestFragment();
+                return requestFragment;
+
+            case 1:
+                ChatFragment chatFragment = new ChatFragment();
+                return  chatFragment;
+
+            case 2:
+                FriendsFragment friendsFragment = new FriendsFragment();
+                return friendsFragment;
+
+            default:
+                return  null;
+        }
+
+    }
+
+    @Override
+    // we need 3 tabs so getCount must return 3
+    public int getCount() {
+        return 3;
+    }
+
+    public CharSequence getPageTitle(int position){
+
+        switch (position) {
+            case 0:
+                return "REQUESTS";
+
+            case 1:
+                return "CHATS";
+
+            case 2:
+                return "FRIENDS";
+
+            default:
+                return null;
+        }
+
+    }
+
+}
